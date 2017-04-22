4eb18dcd2ab8d0f3820464978b353571722f2d9f
Melvin Gruschow <melvin.f.gruschow@gmail.com>
2017-04-22 08:53:08 -0700

    add some of the basic files and folders, including linking the database, setting up the 2 basic controllers, and the product and context models

---
+++ src/GummiBearKingdom/Models/GBKingdomContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GummiBearKingdom.Models
+{
+    public class GBKingdomContext : DbContext
+    {
+        public virtual DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=GBKingdom;integrated security=True");
+        }
+    }
+}
---
+++ src/GummiBearKingdom/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GummiBearKingdom.Models
+{
+    [Table("Products")]
+    public class Product
+    {
+        [Key]
+        public int id { get; set; }
+        public string product_name { get; set; }
+        public int cost { get; set; }
+        public string country { get; set; }
+    }
+}
