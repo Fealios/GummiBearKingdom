3849e77c325caf0f00814d9c47e6d602ce601bb6
Melvin Gruschow <melvin.f.gruschow@gmail.com>
2017-04-22 09:01:19 -0700

    add details route

--- src/GummiBearKingdom/Controllers/ProductController.cs
+++ src/GummiBearKingdom/Controllers/ProductController.cs
@@ -5,16 +5,20 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using GummiBearKingdom.Models;
 
-// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace GummiBearKingdom.Controllers
 {
     public class ProductController : Controller
     {
-        // GET: /<controller>/
+        GBKingdomContext db = new GBKingdomContext();
         public IActionResult Index()
         {
-            return View();
+            return View(db.Products.ToList());
+        }
+        public IActionResult Details(int id)
+        {
+            var thisProduct = db.Products.FirstOrDefault(products => products.id == id);
+            return View(thisProduct);
         }
     }
 }
