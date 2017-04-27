eb6dafdd50fd435f043440b9113e98e955b582d1
Melvin Gruschow <melvin.f.gruschow@gmail.com>
2017-04-26 18:03:07 -0700

    moving readme to src

---
+++ src/GummiBearKingdom/Migrations/20170426235132_clone.Designer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using GummiBearKingdom.Models;
+
+namespace GummiBearKingdom.Migrations
+{
+    [DbContext(typeof(GBKingdomContext))]
+    [Migration("20170426235132_clone")]
+    partial class clone
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rtm-21431")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("GummiBearKingdom.Models.Product", b =>
+                {
+                    b.Property<int>("id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("cost");
+
+                    b.Property<string>("country");
+
+                    b.Property<string>("product_name");
+
+                    b.HasKey("id");
+
+                    b.ToTable("Products");
+                });
+        }
+    }
+}
---
+++ src/README.md
@@ -0,0 +1,19 @@
+# Gummi Bear Kingdom!
+
+### Description:
+Gummi Bear Kingdom is a .net core application created by Melvin Gruschow, Copyright April 26th, 2017, for the week 16 end of week project for Epicodus.  Tech:
+  - C#
+  - .NET Core 1.0
+  - Bootstrap
+
+#### Instructions:
+
+Download this Github repository, and use a Visual Studio version 2015 or later to open the project file located in the root directory of said repository.  Right click on the top level file labeled as 'Solution "GummiBearKingdom"' and click build.  Open a powershell window and navigate into the repository, then navigate into the src/GummiBearKingdom file.  Run the following commands:
+  - dotnet ef migrations add Production
+  - dotnet ef database update
+
+Now run the project by clicking the green arrow at the top of VS, and enjoy.  
+
+
+#### Reference:
+For inspiration, I attempted to mimic the mylittlepony website.  I used mostly the color scheme and some of the other aesthetic for operation, as the site functionality was fairly complex and I didnt think I could recreate it accurately with my basic CSS skills and Bootstrap.  
